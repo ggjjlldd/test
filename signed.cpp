6f5b3a15c53b56fc41e164840811a77ccf70d747
ggjjlldd <ggjjlldd@163.com>
2014-03-09 19:05:27 +0800

    m

---
+++ signed.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  signed char a=-8;
+  cout<<(int)a<<endl<<(int)((unsigned char)a)<<endl;
+  unsigned char b=30;
+  cout<<(int)b<<endl;
+  a=b-(unsigned char)a;
+  cout<<(int)a<<endl;
+  char *p1 = (char *)0;
+  int *p2 = 
+}
